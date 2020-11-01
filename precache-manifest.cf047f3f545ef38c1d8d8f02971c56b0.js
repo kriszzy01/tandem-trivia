ea887cd95cd61000e5c87b431ebaa67316eac5f8
Christian Otu <christianotu.d@gmail.com>
2020-11-01 08:05:50 +0100

    Updates

---
+++ precache-manifest.cf047f3f545ef38c1d8d8f02971c56b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099f54992885ee67b4d15b5463ac43bc",
+    "url": "/tandem-trivia/index.html"
+  },
+  {
+    "revision": "d7e9bd4d68afe6837877",
+    "url": "/tandem-trivia/static/css/main.e8ac03f6.chunk.css"
+  },
+  {
+    "revision": "af0dec12b1a9f25dcd22",
+    "url": "/tandem-trivia/static/js/2.9bc3a455.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tandem-trivia/static/js/2.9bc3a455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e9bd4d68afe6837877",
+    "url": "/tandem-trivia/static/js/main.e4be30ec.chunk.js"
+  },
+  {
+    "revision": "946ef9bd737f8f9a864e",
+    "url": "/tandem-trivia/static/js/runtime-main.7c797ec0.js"
+  }
+]);
