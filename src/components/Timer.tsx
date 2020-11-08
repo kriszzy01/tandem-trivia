bfcd06db6effa6dbc1f039a3036935832807402b
Christian Otu <christianotu.d@gmail.com>
2020-11-08 19:33:40 +0100

    Refactored Timer to typescript

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,24 +1,24 @@
-import React, { useLayoutEffect, useState } from "react";
-//import { actionTypes } from "../utils";
+import React, { useLayoutEffect } from "react";
 import { useGameState } from "../context";
 
 export const Timer: React.FC = () => {
-    const [{ answerSelected }, dispatch] = useGameState();
-
-    const [timeLeft, setTimeLeft] = useState(8);
+    const [{
+        answerSelected,
+        timeElapsed
+    }, dispatch] = useGameState();
 
     const fullTimeScale: number = 849.37; //Gotten from the perimeter of the SVG circle, with Radius = 135.25
 
-    const getTimeRatio = (): number => (timeLeft / 8) * fullTimeScale - 0.1;
+    const getTimeRatio = (): number => (timeElapsed / 8) * fullTimeScale - 0.1;
 
     useLayoutEffect(() => {
         const startTimer = setTimeout(() => {
-            if (timeLeft !== 0 && answerSelected === undefined) {
-                setTimeLeft(timeLeft - 1);
+            if (timeElapsed !== 0 && answerSelected === undefined) {
+                dispatch({ type: "RUN_TIMER" })
             }
         }, 1000);
 
-        if (!answerSelected && timeLeft === 0) {
+        if (!answerSelected && timeElapsed === 0) {
             dispatch({ type: "TIME_ELAPSED" });
             dispatch({ type: "SELECT_ANSWER", payload: false })
         }
@@ -26,7 +26,7 @@ export const Timer: React.FC = () => {
         return () => {
             clearTimeout(startTimer);
         }
-    }, [timeLeft, dispatch, answerSelected]);
+    }, [timeElapsed, dispatch, answerSelected]);
 
     return (
         <div className="timer">
@@ -34,11 +34,11 @@ export const Timer: React.FC = () => {
                 <g fill="#fff" strokeLinecap="round" strokeWidth="25">
                     <circle cx="149.72" cy="153.06" r="135.25" stroke="rgb(9, 94, 67)" />
                     <path d="m284.97 153.06a135.25 135.25 0 0 1-135.25 135.25 135.25 135.25 0 0 1-135.25-135.25 135.25 135.25 0 0 1 135.25-135.25 135.25 135.25 0 0 1 135.25 135.25z"
-                        stroke={timeLeft !== 0 ? "#1af6b3ff" : "#e31227fc"} strokeDasharray={`${getTimeRatio()} ${fullTimeScale}`} />
+                        stroke={timeElapsed !== 0 ? "#1af6b3ff" : "#e31227fc"} strokeDasharray={`${getTimeRatio()} ${fullTimeScale}`} />
                 </g>
             </svg>
-            <span className={`timer__label | font-base ${timeLeft === 0 ? "color-warning" : "color-tertiary-shade"}`}>
-                {timeLeft !== 0 ? timeLeft : "Time Up!"}
+            <span className={`timer__label | font-base ${timeElapsed === 0 ? "color-warning" : "color-tertiary-shade"}`}>
+                {timeElapsed !== 0 ? timeElapsed : "Time Up!"}
             </span>
         </div>
     );
